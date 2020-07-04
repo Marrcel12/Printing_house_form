19f6602a07e5e1ee6b6c8165a138091f78cbd3d8
marcellap <kkk>
2020-07-04 23:00:03 +0200

    session managment and baner path

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,session
 from test import SignUpForm
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'qaz123'
 app.config['TEMPLATES_AUTO_RELOAD'] = "True"
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 @app.route('/start')
-def test():
-    return render_template('start.html')
+def start():
+    return render_template('start.html')    
 
 @app.route('/products/<url>')
 def products(url):
+    
     product=url
+    session["product"] = product 
+
     if product == 'baner':
         materials=[['FRONTLIT',"Idealny do zastosowania na zewnątrz"],['MESH (SIATKA)','Idealny do zastosowania na zewnątrz, na dużych powierzchniach lub miejscach o dużym nasileniu wiatru'],['BLOCKOUT', 'Idealny do wyeksponowania oferty po dwóch stronach baneru'],['POLIESTER 205g','Idealny do zastosowania wewnątrz'],['Poliester 115g','Idealny do zastosowania wewnątrz i do flag']]
     if product == 'sticker':
@@ -30,6 +32,26 @@ def products(url):
 
 @app.route('/products/materials/<url>' )
 def materials(url):
-    return(url)
-
+    
+    product=url
+    session["material"] = product
+    methods=""
+    if product == 'FRONTLIT':
+        methods=["ZGRZEW","DOCIĘTE NA WYMIAR"]
+    if product == 'MESH (SIATKA)':
+        methods=["ZGRZEW + OCZKA"]
+    if product == 'BLOCKOUT':
+        methods=['DOCIĘTE NA WYMIAR']
+    if product == 'POLIESTER 205g':
+        methods=['OBSZYCIE']
+    if product == 'Poliester 115g':
+        methods=['DOCIĘTE NA WYMIAR','OBSZYCIE + OCZKA']
+    return render_template('materials.html',product=product,materials=methods )
+@app.route('/products/materials/methods/<url>')
+def details(url):
+    product=url
+    session['method'] = product
+    for i in session:
+        print("sesion item name:",i)
+    return("hhh")
 app.run()
