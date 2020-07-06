4b92df1cb74747e4deac79263e85cfce76647deb
Adrian Jobda <31901782+theConsite@users.noreply.github.com>
2020-07-06 12:53:04 +0200

    changes...

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def products(url):
     if product == 'baner':
         materials=[['FRONTLIT',"Idealny do zastosowania na zewnątrz"],['MESH (SIATKA)','Idealny do zastosowania na zewnątrz, na dużych powierzchniach lub miejscach o dużym nasileniu wiatru'],['BLOCKOUT', 'Idealny do wyeksponowania oferty po dwóch stronach baneru'],['POLIESTER 205g','Idealny do zastosowania wewnątrz'],['Poliester 115g','Idealny do zastosowania wewnątrz i do flag']]
     if product == 'sticker':
-        materials=[['FOLIA BłYSZCZĄCA'],['FOLIA MATOWA'],['EASY DOT', "Folia umożliwiająca przeniesienie wydruku w inne miejsce bez utraty właściwości klejących materiału"], ["MAGNEZ BŁYSZCZĄCY"]]
+        materials=[['FOLIA BłYSZCZĄCA'],['FOLIA MATOWA'],['EASY DOT', "Folia umożliwiająca przeniesienie wydruku w inne miejsce bez utraty właściwości klejących materiału"], ["MAGNEZ BŁYSZCZĄCY"], ["MAGNES MATOWY"]]
     if product == 'pad':
         materials=[['DYWAN','Idealny do zastosowania np. jako wycieraczka'],['FLOORPROMOTOR','Idealny do zastosowania np. jako podkładka pod myszkę']]
     if product == 'poster':
@@ -33,25 +33,53 @@ def products(url):
 @app.route('/products/materials/<url>' )
 def materials(url):
     
-    product=url
-    session["material"] = product
+    material=url
+    session["material"] = material
     methods=""
-    if product == 'FRONTLIT':
+    if material == 'FRONTLIT':
         methods=["ZGRZEW","DOCIĘTE NA WYMIAR"]
-    if product == 'MESH (SIATKA)':
+    if material == 'MESH (SIATKA)':
         methods=["ZGRZEW + OCZKA"]
-    if product == 'BLOCKOUT':
+    if material == 'BLOCKOUT':
         methods=['DOCIĘTE NA WYMIAR']
-    if product == 'POLIESTER 205g':
+    if material == 'POLIESTER 205g':
         methods=['OBSZYCIE']
-    if product == 'Poliester 115g':
+    if material == 'Poliester 115g':
         methods=['DOCIĘTE NA WYMIAR','OBSZYCIE + OCZKA']
-    return render_template('materials.html',product=product,materials=methods )
+    return render_template('materials.html',material=material,methods=methods )
+
+
 @app.route('/products/materials/methods/<url>')
-def details(url):
-    product=url
-    session['method'] = product
-    for i in session:
-        print("sesion item name:",i)
-    return("hhh")
+def methods(url):
+    method=url
+    session['method'] = method
+    item =session['product']
+    if item == 'baner':
+        sizing = "0"
+    if item !='baner':
+        sizing = ['220x20', '300x30']
+    return render_template('size.html', sizing=sizing)
+
+@app.route('/products/materials/methods/file/<url>')
+def file(url):
+    size = url.split('!')[0]
+    quantity = url.split('!')[1]
+    session['size'] = size
+    session['quantity'] = quantity
+    return render_template('file.html')
+
+@app.route('/products/materials/methods/file/data/<url>')
+def data(url):
+    urlSplitted = url.split("!")
+    session["project"] =urlSplitted[0]
+    if(urlSplitted[1]=='l'):
+        session['plikType'] = 'link'
+        plik = bytearray.fromhex(urlSplitted[2]).decode()
+    else:
+        session['plikType'] = 'file'
+        plik = urlSplitted[2]
+
+    session["file"] = plik
+    return render_template('data.html')
+
 app.run()
