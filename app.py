56f3115043e186d6eed7407d2e0b7bd0e4ead943
kkk <mmma@m>
2020-07-15 10:05:04 +0200

    login admin panel repair

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,session
+from flask import Flask, render_template, request,session,redirect,url_for
 from test import SignUpForm
 from flask_mail import Mail, Message
 import base64
@@ -16,6 +16,19 @@ app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
+# login 
+class User:
+    def __init__(self,id,username,password):
+        self.id=id
+        self.username=username
+        self.password=password
+    def __repr__(self):
+        return f'<User: {self.username}'
+users=[]
+users.append(User(id=1,username="admin",password="qaz123"))
+print(users)
+
+# database
 def bazkie_produkty(sql):
     conn = psycopg2.connect(database="drukarnia", user = "postgres", password = "qaz123", host = "127.0.0.1", port = "5432")
     cur = conn.cursor()
@@ -136,19 +149,26 @@ def submit(url):
     mail.send(msg)
     return render_template('submit.html')
 
-@app.route('/login')
+@app.route('/login', methods= ['GET','POST'])
 def login():
-    session['login']=0
+    if request.method =='POST':
+        session.pop('user_id',None)
+        password = request.form['password']
+        users_pass=[x for x in  users if  x.password == password]
+        if len(users_pass) >=1:
+            return redirect(url_for('adminpanel'))
+        else:
+            session['login']=0
+            return render_template('login.html', wrong=1)
+            
     return render_template('login.html', wrong=0)
 
-@app.route('/adminpanel/<url>')
-def adminpanel(url):
-    if(url == "qaz123"):
+@app.route('/adminpanel')
+def adminpanel():
         products= bazkie_produkty('select name from public."Produkty"')
         session["login"]=1
         return render_template('adminpanel.html' ,produkty = products)
-    else:
-        return render_template('login.html', wrong=1)
+    
 
 @app.route('/addproduct')
 def addproduct():
